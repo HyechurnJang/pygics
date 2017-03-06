d56cfa78d6e871eaf6b3c4e66a408b9275e495d1
Hye-Churn, Jang <hyjang@cisco.com>
2017-03-06 19:08:05 +0900

    0.1.2 Release

--- setup.py
+++ setup.py
@@ -5,25 +5,13 @@ def read(fname): return open(os.path.join(os.path.dirname(__file__), fname)).rea
 
 setup(
     name='pygics',
-    version='0.1.1',
+    version='0.1.2',
     license='Apache 2.0',
     author='Hyechurn Jang',
     author_email='hyjang@cisco.com',
     url='https://github.com/HyechurnJang/pygics',
-    description='Python Service Library',
+    description='Python Gevent Interface for Coresponding Service',
     long_description=read('README'),
     packages=['pygics'],
-    classifiers=[
-      'Development Status :: 4 - Beta',
-      'License :: OSI Approved :: Apache Software License',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 2',
-      'Programming Language :: Python :: 2.7',
-      'Topic :: Software Development :: Libraries :: Python Modules',
-      'Operating System :: POSIX',
-      'Operating System :: POSIX :: Linux',
-      'Operating System :: MacOS',
-      'Operating System :: MacOS :: MacOS X',
-    ],
-    install_requires=[]
+    install_requires=['gevent', 'requests']
 )
